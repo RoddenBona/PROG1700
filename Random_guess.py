b1e36f7a8175174ff270744c412addef5d74ef99
RoddenBona <146233859+RoddenBona@users.noreply.github.com>
2023-10-16 14:38:05 -0300

    need to add notes

--- Random_guess.py
+++ Random_guess.py
@@ -1,16 +1,19 @@
 import random
 
 print("Guess which number I am thinking about")
+print("You only have 10 tries to figure it out")
 invalid_attempts = 0
+
+cpunumb = random.randrange(1,11)
 
 while invalid_attempts <= 5:
     userchoice = input("Enter Number: ")
     if userchoice.isdigit():
         userchoice = int(userchoice)
         if userchoice > 0:
-            cpunumb = random.randrange(1,11)
             if userchoice == cpunumb:
                 print ("Correct!")
+                print("You guessed " + str(cpunumb) + " in" + str(invalid_attempts) + " attempts")
             else:
                 if userchoice > cpunumb:
                     print("Too high!")
