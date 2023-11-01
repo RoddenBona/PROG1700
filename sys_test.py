73baaa20dde9f5ccde6fdb89e363f6b6c96d84b0
RoddenBona <W0461260@campus.nscc.ca>
2023-11-01 14:40:52 -0300

    idk what this is for

--- sys_test.py
+++ sys_test.py
@@ -1,5 +1,5 @@
-import sys
+parsecs_input = input("Input number of parsecs:")
+parsecs = int(parsecs_input)
+lightyears = 3.26156 * parsecs
 
-print(sys.argv)
-print(sys.argv[0]) # program name
-print(sys.argv[1]) # first arg
+print(parsecs_input + " parsecs is " + str(lightyears) + " lightyears")
