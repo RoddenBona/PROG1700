f59664182f3ff975090fbf125f7b26a94c2d3e1f
RoddenBona <W0461260@campus.nscc.ca>
2023-11-16 15:17:25 -0400

    Quiz complete

--- w0461260_LIST_Quiz.py
+++ w0461260_LIST_Quiz.py
@@ -12,24 +12,38 @@ random.shuffle(quiz_data)
 #player score variable 
 score = 0
 
-
+#use the difficulty and laguages for the for loop of the quiz
 for language, difficulty in quiz_data:
+    #user guesses the difficulty of the prompted language
     guess = input(f"""What is the difficulty of {language}?
                   Enter a number from 1-5: """)
+    #convert answer to int if it's a number
     if guess.isdigit():
         guess = int(guess)
+        #if answer is correct. add a point to the score
         if guess == difficulty:
             print("Correct!")
             score = score + 1
+        #result if answer is incorrect
         else:
             print(f"Oops! The correct difficulty of {language} is {difficulty}")
+    #message if input is not a number
     else:
-        print("Invalid entry. PLease enter a number")
+        print("Oops. You have to enter a number")
 
+#once quiz is complete
 else:
+    
     print("Quiz over! You end with a score of",score)
-    if score == 5:
-        print("Amazing! You got a perfect score!")
+#if the user got a 0 by answering with non int values the whole time
+    if score == 0:
+        print("Buddy... You gotta answer with numbers,What are you doing?")
     else:
-        print("you got",score,"out of 5 questions correct")
-        print("Play again sometime! Try for that pefect score!")
+        #if user got a perfect score on the quiz
+        if score == 5:
+            print("Amazing! You got a perfect score!")
+        # if user did not get a perfet score. tell them how many they did get.
+        #and encourage them to try again for a perfect score
+        else:
+            print("you got",score,"out of 5 questions correct")
+            print("Play again sometime! Try for that pefect score!")
