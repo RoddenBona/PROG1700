7c5f086ce391eb9f9b5ca55eb87d923c3bee02cf
RoddenBona <W0461260@campus.nscc.ca>
2023-11-20 15:01:34 -0400

    Next objecive: updatiting

--- w0461260_student_manager.py
+++ w0461260_student_manager.py
@@ -1,8 +1,8 @@
 students = ["Abby","Brandon","Carly","Damien","Eddie"]
 student_id = [1,2,3,4,5]
-grades = [80,90,79,70,85]
+student_grades = [80,90,79,70,85]
 
-database = list(zip(students,student_id,grades))
+database = list(zip(students,student_id,student_grades))
 
 while True:
     choice = input("""Please select an option:
@@ -21,7 +21,9 @@ while True:
                 new_grade = input("Enter new student grade:")
                 if new_grade.isdigit():
                     new_grade = int(new_grade)
-                    
+                    students.append(new_name)
+                    student_id.append(new_id)
+                    student_grades.append(new_grade)
                     print("Student has been added to the list")
                 else:
                     print("Please enter grade as a number")
@@ -30,10 +32,10 @@ while True:
             
         else:
             if choice == 2:
-                for names in students:
-                    print(f"Name:{names}")
+                for names,ID,grades in students:
+                    print(f"Name:{names} ID:{ID} Grade:{grades}")
             else:
                 if choice == 3:
                     update_option = input("Enter a student name")
                     if update_option in students:
-
+                        update_option
