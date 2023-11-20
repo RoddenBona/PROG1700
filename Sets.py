3eb3e8dd9f38cbce0a7f5db20fb93f7bfdb2937c
RoddenBona <W0461260@campus.nscc.ca>
2023-11-20 09:45:26 -0400

    lesson not needed

--- Sets.py
+++ Sets.py
@@ -1,1 +1,10 @@
-#Python sets lesson?
+#Python sets lesson
+
+#Used to store multiple items in a single variable much like lists, 
+# except there is no ordered index, they're unchangeable, you can't have duplicates
+# the only way to change an item in a set is to outright add or remove it
+
+#Example
+
+newset = {"Mario","Geno","Mallow"}
+print(newset)
