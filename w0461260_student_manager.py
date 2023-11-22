2e84de680b9d0d8a0c354515313530d2ac171624
RoddenBona <W0461260@campus.nscc.ca>
2023-11-22 14:37:13 -0400

    Trying to edit lists either does nothing or crash

--- w0461260_student_manager.py
+++ w0461260_student_manager.py
@@ -10,15 +10,16 @@ while True:
                 2)See all current records
                 3)update a current record
                 4)Delete a current record
-                5)Exit""")
+                5)Exit
+                Enter option here: """)
     if choice.isdigit():
         choice = int(choice)
         if choice == 1:
             new_name = input("Enter new student name: ")
-            new_id = input("Enter new student id")
+            new_id = input("Enter new student id: ")
             if new_id.isdigit():
                 new_id = int(new_id)
-                new_grade = input("Enter new student grade:")
+                new_grade = input("Enter new student grade: ")
                 if new_grade.isdigit():
                     new_grade = int(new_grade)
                     students.append(new_name)
@@ -32,10 +33,39 @@ while True:
             
         else:
             if choice == 2:
-                for names,ID,grades in students:
+                for names,ID,grades in database:
                     print(f"Name:{names} ID:{ID} Grade:{grades}")
             else:
                 if choice == 3:
-                    update_option = input("Enter a student name")
+                    update_option = input("Enter a student name to update: ")
                     if update_option in students:
-                        update_option
+                        student_index = students.index(update_option)
+                        updated = input("Enter new student grade")
+                        if updated.isdigit():
+                            updated = int(updated)
+                        else:
+                            print("Invalid input")
+                else:
+                    if  choice == 4:
+                        deletion_option = input("Enter a student name to remove: ")
+                        if deletion_option in students:
+                            deletion_index = int(students.index(deletion_option))
+                            final_option = input("""Do you really wish to delete this entry?
+                                                 1)Yes
+                                                 2)No
+                                                 Enter option: """)
+                            if final_option.isdigit():
+                                final_option = int(final_option)
+                                if final_option == 1:
+                                    students.remove(deletion_index)
+                                    student_id.remove(deletion_index)
+                                    student_grades.remove(deletion_index)
+                                else:
+                                    if final_option == 2:
+                                        print("Deletion program aborted")
+                            else:
+                                print("nvalid entry")
+                    else:
+                        if choice == 5:
+                            print("Exiting program. Thank you for your time")
+                            break
