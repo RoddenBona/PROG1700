08fdcb25c3530eafcfe5f227bfef3a4cce428bdd
RoddenBona <W0461260@campus.nscc.ca>
2023-11-23 15:28:26 -0400

    report one is currentl being a big pain in my ass

--- w0461260_PROG1700-Assignment_Order_and_Customer_Management_System_In_Python.py
+++ w0461260_PROG1700-Assignment_Order_and_Customer_Management_System_In_Python.py
@@ -10,7 +10,7 @@ Created: Nov 22 2023
 Last edited Nov 22 2023
 """
 
-customers = {1: {"name": "Alice", "contact": "123-456-7890", "orders": ["hey","this is","a test"]},
+customers = {1: {"name": "Alice", "contact": "123-456-7890", "orders": []},
             2: {"name": "Bob", "contact": "987-654-3210", "orders": []},
             }
 
@@ -18,7 +18,7 @@ number_of_entries = len(customers)
 
 def customer_addition():
     while True:
-        add_option = input("""Do you wish to add a ne customer?
+        add_option = input("""Do you wish to add a new customer?
                         1)Yes
                         2)No
                         Enter option here: """)
@@ -30,34 +30,45 @@ def customer_addition():
                 new_id = number_of_entries + 1
                 customers[new_id] = {"name":add_fname, "contact":add_contact, "orders":[]}
                 print("New entry has been added")
-                main_menu()
+                break
             else:
                 if add_option == 2:
                     print("Exiting to main menu")
-                    main_menu()
+                    break
                 else:
                     print("invalid entry. Please enter option as a number")
         else:
             print("Invalid entry. Please enter a number")
 
 def order_placement(customerID, product_name, quantity, unit_price):
+    number_of_orders = len(customers[customerID]["orders"])
     while True:
-        order_choice = input("""Would you like to place a new order for this customer?""")
+        order_choice = input("""Would you like to place a new order for this customer?
+                             1)Yes
+                             2)No
+                             Enter option here: """)
         if order_choice.isdigit():
             order_choice = int(order_choice)
             if order_choice == 1:
-                print("test")
-                main_menu()
+                new_id = number_of_orders + 1
+                new_order = {"OrderID":new_id, "Product":product_name, "Quantity":quantity, "Price":unit_price}
+                customers[customerID]["orders"].append(new_order)
+                print("Order has been added")
+                break
             else:
                 if order_choice == 2:
                     print("Exiting to main menu")
-                    main_menu()
+                    break
                 else:
                     print("Invalid entry. Option not in list")
         else:
             print("Invalid entry. Please enter a number")
 
 def report_one(customerID):
+    total_spending = 0
+    customer = customers.get(customerID)
+
+
     while True:
         report_option = input("""Do you wish to generate a report of the selected customer?
                               1)Yes
@@ -66,12 +77,18 @@ def report_one(customerID):
         if report_option.isdigit():
             report_option = int(report_option)
             if report_option == 1:
-                print("test. You inputed", customerID)
-                main_menu()
+                if customer:
+                    for order in customer("orders"):
+                        total_spending += order["Price"]
+                    print(f"Total Spending: ${total_spending}")
+                    print("Orders:")
+                    for order in dict(customer["orders"]):
+                        print(f"{order}")
+                break
             else:
                 if report_option == 2:
                     print("Exiting to main menu")
-                    main_menu()
+                    break
                 else:
                     print("Invalid entry. Option not in list")
         else:
@@ -87,51 +104,70 @@ def report_all():
             report_choice = int(report_choice)
             if report_choice == 1:
                     print(customers)
-                    main_menu()
+                    break
             else:
                 if report_choice == 2:
                     print("Exiting to main menu")
-                    main_menu()
+                    break
                 else:
                     print("invalid entry. Option not in list")
         else:
             print("Invalid entry. Please enter a number")
 
-def main_menu():
-    while True:
-        menu_choice = input("""Please select an option
-                            1)Add New customer
-                            2)Place order
-                            3)Generate report
-                            4)Generate all reports
-                            5)Exit
-                            Enter option here: """)
-        if menu_choice.isdigit():
-            menu_choice = int(menu_choice)
-            if menu_choice == 1:
-                customer_addition()
-            else:
-                if menu_choice == 2:
-                    order_placement()
-                else:
-                    if menu_choice == 3:
-                        customer_search = input("Enter CustomerID")
-                        if customer_search in customers:
-                            customerID = customer_search
-                            report_one(customerID)
-                    else:
-                        if menu_choice == 4:
-                            report_all()
-                        else:
-                            if menu_choice == 5:
-                                print("Exiting program. Thank you for your time")
-                                break
-                            else:
-                                print("Invalid entry. Option not in list")
+while True:
+    menu_choice = input("""Please select an option
+                        1)Add New customer
+                        2)Place order
+                        3)Generate report
+                        4)Generate all reports
+                        5)Exit
+                        Enter option here: """)
+    if menu_choice.isdigit():
+        menu_choice = int(menu_choice)
+        if menu_choice == 1:
+            customer_addition()
         else:
-            print("Invalid entry. Please enter a number")
-
-main_menu()
+            if menu_choice == 2:
+                customer_search = input("Enter CustomerID: ")
+                if customer_search.isdigit:
+                    customer_search = int(customer_search)
+                    if customer_search in customers:
+                        customerID = customer_search
+                        product_name = input("Enter product name: ")
+                        quantity = input("Enter quantity of product: ")
+                        if quantity.isdigit():
+                            quantity = int(quantity)
+                            unit_price = input("Enter price of the product (No decimals): ")
+                            if unit_price.isdigit():
+                                unit_price = int(unit_price)
+                                order_placement(customerID,product_name,quantity,unit_price)
+                            else:
+                                print("Invalid entry")
+                        else:
+                            print("Invalid entry")
+                    else:
+                        print("Invalid entry")
+                else:
+                    print("Invalid entry")
+            else:
+                if menu_choice == 3:
+                    customer_search = input("Enter CustomerID")
+                    if customer_search.isdigit():
+                        customer_search = int(customer_search)
+                    if customer_search in customers:
+                        customerID = customer_search
+                        report_one(customerID)
+                else:
+                    if menu_choice == 4:
+                        report_all()
+                    else:
+                        if menu_choice == 5:  
+                            print("Exiting program. Thank you for your time")
+                            break
+                        else:
+                            print("Invalid entry. Option not in list")
+    else:
+        print("Invalid entry. Please enter a number")
 
 # This is how to print/access the list inside of the dictionary
 # Which is in another dictionary itself 
