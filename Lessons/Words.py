e500aa78af79d15bef7179d6b208c58489a30ffa
RoddenBona <W0461260@campus.nscc.ca>
2023-11-29 14:01:33 -0400

    More folders made for sorting

---
+++ Lessons/Words.py
@@ -0,0 +1,3 @@
+names = ["Matt", "Harper" ,"Tania", "Carson", "Porter", "Piper"]
+for name in names[::-1]:
+    print(name [::-1])
--- Words.py
+++
@@ -1,3 +0,0 @@
-names = ["Matt", "Harper" ,"Tania", "Carson", "Porter", "Piper"]
-for name in names[::-1]:
-    print(name [::-1])
