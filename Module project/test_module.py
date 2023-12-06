5487dc5b85d9a17640fe72f8d0130390eb1dbfdc
RoddenBona <W0461260@campus.nscc.ca>
2023-12-06 14:06:46 -0400

    Testing moules in python

---
+++ Module project/test_module.py
@@ -0,0 +1,11 @@
+#This is wht allows us to import other funccction into our own code
+#When we import things like random and datetime. Those are just other python files
+#That were made y other people being inported for us to just inject into our code
+
+
+#As a simple example. we will define a greeting function to just say hello to us when something is inputed into "name"
+def greeting(name):
+    print(f"Hello there {name}")
+
+#Now that the function is defined. We can import it into another one of our files
+#Check the second test_module file for that in use
---
+++ Module project/test_module_2.py
@@ -0,0 +1,10 @@
+#Here in this file. We will import the first test_module into this one for use to use the
+#greeting function that we defined in there
+import test_module
+
+#So now we just call on the file. Specifying which function of the file to use
+#and inputing something into the "name" variable of the greeting function
+test_module.greeting("Rodden")
+
+name = "Jonah"
+test_module.greeting(name)
