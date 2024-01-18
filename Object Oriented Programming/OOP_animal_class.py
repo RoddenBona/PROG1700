e45bf42d92f7da3178b7007a40c5bc144337a850
RoddenBona <W0461260@campus.nscc.ca>
2024-01-18 12:20:29 -0400

    OOP

--- Object Oriented Programming/OOP_animal_class.py
+++ Object Oriented Programming/OOP_animal_class.py
@@ -28,3 +28,48 @@ cat = Cat("Finn")
 print(dog.name)
 print(cat.name)
 print(F"{dog.speak()} and {cat.speak()}")
+
+
+#Polymorphism in python allows objects of different types to inhierit objects of a common type
+
+class Fruit:
+    def __init__(self,name,):
+        self.name = name
+
+    #Method to return "generic taste"
+    def taste(self):
+        return "Generic Taste"
+
+class Apple(Fruit):
+    def __init__ (self, name, color):
+    #Super is a built in function used usually in OOP
+    #Used specifically within classes that are part of an inhieritance hierarchy
+    #It provides a way to access and call methods from a superclass (parent class)
+    #in a subclass (child class)
+
+        super().__init__(name)
+        self.color = color
+
+    def taste(self):
+        generic_taste = super().taste() #Calling the superclass method
+        return f"{generic_taste} and Crispy"
+    
+class Orange(Fruit):
+    def __init__ (self, name, size):
+        super().__init__(name)
+        self.size = size
+
+    def taste(self):
+        generic_taste = super().taste() #Calling the superclass method
+        return f"{generic_taste} and Juicy"
+
+def describe_taste(Fruit):
+    print (f"{Fruit.name} tastes: {Fruit.taste()}")
+
+#make instances of Apples and Oranges
+apple = Apple("Honeydew", "Yellow")
+orange = Orange("Orange Orange", "Large")
+
+#Demonstrate polymorphism by calling the describe taste function with different fruit objects
+describe_taste(apple)
+describe_taste(orange)
